0cf52aa661907f4d9b4fb8d11429e732bc193d83
Javohir004 <javohirsharifjonov795@gmail.com>
2024-11-11 15:35:02 +0500

    ko'p xatolarni to'g'irladim

--- src/test/java/uz/jvh/uzairways/UzAirWaysApplicationTests.java
+++ src/test/java/uz/jvh/uzairways/UzAirWaysApplicationTests.java
@@ -1,13 +1,13 @@
-package uz.jvh.uzairways;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class UzAirWaysApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package uz.jvh.uzairways;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class UzAirWaysApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
